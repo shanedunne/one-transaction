a76b7299ff5dc1a7fb660cc7f60319ff42fb9278
shanedunne <shanejjdunne@hotmail.com>
2022-09-09 16:45:29 +0200

    add hardhat configuration for goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `${process.env.ALCHEMY_GOERLI_URL}`,
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
+  defaultNetwork: "goerli",
+  etherscan: {
+    apiKey: "3F8MYM7X3V38SQPT4NCSV61ZZHVYT3NDCI",
+  },
 };
